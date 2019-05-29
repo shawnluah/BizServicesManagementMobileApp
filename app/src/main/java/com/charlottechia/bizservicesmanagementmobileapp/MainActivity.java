84a518450ff64b4bf591f7d4a36dea8c58927dc9
shawn luah <17010564@myrp.edu.sg>
2019-05-29 11:46:02 +0800

    y

--- app/src/main/java/com/charlottechia/bizservicesmanagementmobileapp/MainActivity.java
+++ app/src/main/java/com/charlottechia/bizservicesmanagementmobileapp/MainActivity.java
@@ -1,14 +1,17 @@
 package com.charlottechia.bizservicesmanagementmobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView etUsername,etPassword;
+    EditText etUsername,etPassword;
     Button btnLogin;
 
     @Override
@@ -22,5 +25,29 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password = etPassword.getEditableText().toString();
+                String username = etUsername.getEditableText().toString();
+
+                if ((username.equalsIgnoreCase("1")) && (password.equalsIgnoreCase("password"))) {
+
+                    openAdminHomePage();
+                } else if ((username.equalsIgnoreCase("169799978")) && (password.equalsIgnoreCase("password"))) {
+
+                }
+            }
+        });
+
+    }
+    public void openAdminHomePage () {
+        Intent intent  = new Intent(getBaseContext(), HomeActivity.class);
+        startActivity(intent);
+    }
+    public void openUserHomePage () {
+        Intent intent2  = new Intent(getBaseContext(), HomeActivity.class);
+        startActivity(intent2);
     }
 }
