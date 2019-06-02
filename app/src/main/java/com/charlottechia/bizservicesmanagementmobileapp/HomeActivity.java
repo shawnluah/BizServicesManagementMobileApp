fb96e594f2fd0b3c2f211d647743fe9618792a26
shawn luah <17010564@myrp.edu.sg>
2019-06-02 17:45:44 +0800

    y

--- app/src/main/java/com/charlottechia/bizservicesmanagementmobileapp/HomeActivity.java
+++ app/src/main/java/com/charlottechia/bizservicesmanagementmobileapp/HomeActivity.java
@@ -1,19 +1,51 @@
 package com.charlottechia.bizservicesmanagementmobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button btnMC, btnMT, btnMU;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnMC = findViewById(R.id.btnMC);
+        btnMT = findViewById(R.id.btnMT);
+        btnMU = findViewById(R.id.btnMU);
+
+        btnMC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateClient();
+            }
+        });
+        btnMU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateUser();
+            }
+        });
     }
 
+
+    public void openCreateClient () {
+        Intent intent  = new Intent(getBaseContext(), CreateClientActivity.class);
+        startActivity(intent);
+    }
+    public void openCreateUser () {
+        Intent intent  = new Intent(getBaseContext(), CreateUserActivity.class);
+        startActivity(intent);
+    }
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        MenuInflater inflater = getMenuInflater();
