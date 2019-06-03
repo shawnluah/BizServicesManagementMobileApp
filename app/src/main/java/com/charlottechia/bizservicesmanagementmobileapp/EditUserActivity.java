4a20b8574d1f09c55eafd0ed10d2c76cb3cf3ba4
Haziq200 <17010603@myrp.edu.sg>
2019-06-03 12:51:40 +0800

    Latest

--- app/src/main/java/com/charlottechia/bizservicesmanagementmobileapp/EditUserActivity.java
+++ app/src/main/java/com/charlottechia/bizservicesmanagementmobileapp/EditUserActivity.java
@@ -1,13 +1,34 @@
 package com.charlottechia.bizservicesmanagementmobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditUserActivity extends AppCompatActivity {
+
+    Button btnCanelUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user);
+
+        btnCanelUser = findViewById(R.id.btnCancelCreateCreateUser);
+
+        btnCanelUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                  openUserAdminActivity();
+            }
+        });
+
     }
+
+    public void openUserAdminActivity () {
+        Intent intent  = new Intent(getBaseContext(), UserAdminActivity.class);
+        startActivity(intent);
+    }
+
 }
